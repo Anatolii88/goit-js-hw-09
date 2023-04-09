6159c1965316ceb3cfb7fffd7a143a199a763778
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-04-09 22:02:30 +0300

    Add three task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+//  Отслеживаю форму
+const formEl = document.querySelector('.form')
+
+//  Добавляю слушателя события  на форму 
+formEl.addEventListener('submit', onSubmit)
+
+function onSubmit(evt) {
+  evt.preventDefault()
+
+  let delayStep = Number(formEl.elements.step.value);
+  let delay = Number(formEl.elements.delay.value);
+
+  for (let i = 1; i <= formEl.elements.amount.value; i += 1) { 
+    
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += delayStep;
+  }
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
     // Fulfill
+        resolve({position, delay})
   } else {
     // Reject
+        reject({position, delay})
   }
+   },delay)
+     })
+  
 }
